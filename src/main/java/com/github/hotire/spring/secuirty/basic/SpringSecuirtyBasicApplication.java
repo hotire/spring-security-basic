d38837e246b1a5837d7c10e0a4b9235a466f49ea
hotire <gngh0101@gmail.com>
2019-08-11 01:28:13 +0900

    init

---
+++ src/main/java/com/github/hotire/spring/secuirty/basic/SpringSecuirtyBasicApplication.java
@@ -0,0 +1,13 @@
+package com.github.hotire.spring.secuirty.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuirtyBasicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecuirtyBasicApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/hotire/spring/secuirty/basic/SpringSecuirtyBasicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.hotire.spring.secuirty.basic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecuirtyBasicApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}

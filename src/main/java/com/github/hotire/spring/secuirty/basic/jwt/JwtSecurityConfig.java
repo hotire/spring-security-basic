40a96a2b1131e3680bcde8b3cc666c08dc8fb666
hotire <gngh0101@gmail.com>
2020-03-10 00:31:20 +0900

    Add config of decoder jwt

--- src/main/java/com/github/hotire/spring/secuirty/basic/jwt/JwtSecurityConfig.java
+++ src/main/java/com/github/hotire/spring/secuirty/basic/jwt/JwtSecurityConfig.java
@@ -1,12 +1,18 @@
 package com.github.hotire.spring.secuirty.basic.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.publish-key}")
+    private String publishKey;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.oauth2ResourceServer()
-            .jwt();
+            .jwt()
+            .decoder(RSAJwtDecoder.withPublicKey(publishKey));
     }
 }

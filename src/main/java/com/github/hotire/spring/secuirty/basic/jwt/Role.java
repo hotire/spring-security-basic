1aef83388439f28cf94a7e5b638036b8293a2aa3
hotire <gngh0101@gmail.com>
2020-03-11 00:06:11 +0900

    Add lookup static method

--- src/main/java/com/github/hotire/spring/secuirty/basic/jwt/Role.java
+++ src/main/java/com/github/hotire/spring/secuirty/basic/jwt/Role.java
@@ -1,19 +1,35 @@
 package com.github.hotire.spring.secuirty.basic.jwt;
 
 import com.google.common.collect.Lists;
+import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
+import static java.util.stream.Collectors.toMap;
+
+
+@Getter
 public enum Role {
     USER("USER"),
     ADMIN("ADMIN"),
-    NONE("NONE")
+    NONE("NONE");
+
     private final String role;
     private final List<SimpleGrantedAuthority> authorities;
 
     Role(final String role) {
         this.role = "ROLE_" + role;
         this.authorities = Lists.newArrayList(new SimpleGrantedAuthority(this.role));
+    }
+
+    private static final Map<String, Role> ROLE_MAP = Arrays.stream(Role.values()).collect(toMap(Role::getRole, Function.identity()));
+
+    public static Role lookup(final String role) {
+        return Optional.ofNullable(ROLE_MAP.get(role)).orElse(NONE);
     }
 }

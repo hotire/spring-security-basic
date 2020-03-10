2ece23a4a9d47c07670310264171b2fecba000d0
hotire <gngh0101@gmail.com>
2020-03-10 23:37:24 +0900

    Implement Role

---
+++ src/main/java/com/github/hotire/spring/secuirty/basic/jwt/Role.java
@@ -0,0 +1,18 @@
+package com.github.hotire.spring.secuirty.basic.jwt;
+
+import com.google.common.collect.Lists;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+public enum Role {
+    USER("USER"),
+    ADMIN("ADMIN");
+    private final String role;
+    private final List<SimpleGrantedAuthority> authorities;
+
+    Role(final String role) {
+        this.role = "ROLE_" + role;
+        this.authorities = Lists.newArrayList(new SimpleGrantedAuthority(this.role));
+    }
+}

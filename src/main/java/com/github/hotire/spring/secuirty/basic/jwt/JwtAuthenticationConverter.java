d3ba5d19ced5f17e8349e373ab1a07bfe08131e9
hotire <gngh0101@gmail.com>
2020-03-13 00:17:35 +0900

    Implement jwt convert

--- src/main/java/com/github/hotire/spring/secuirty/basic/jwt/JwtAuthenticationConverter.java
+++ src/main/java/com/github/hotire/spring/secuirty/basic/jwt/JwtAuthenticationConverter.java
@@ -4,15 +4,18 @@ package com.github.hotire.spring.secuirty.basic.jwt;
 import com.google.common.collect.Lists;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.Optional;
 
 public class JwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
-        final String role = jwt.getClaim("role");
-        // TODO Implement role
-        return new JwtAuthenticationToken(jwt, Lists.newArrayList(new SimpleGrantedAuthority(role)));
+        return new JwtAuthenticationToken(jwt,  Optional.ofNullable(jwt.getClaim("role"))
+                                                        .map(Object::toString)
+                                                        .map(Role::lookup)
+                                                        .map(Role::getAuthorities)
+                                                        .orElse(Lists.newArrayList()));
     }
 }

62053819c487a305be3abf883695895dfa3a1ee4
hotire <gngh0101@gmail.com>
2020-03-25 00:11:54 +0900

    Add Nonnull annoation

--- src/main/java/com/github/hotire/spring/secuirty/basic/jwt/JwtAuthenticationConverter.java
+++ src/main/java/com/github/hotire/spring/secuirty/basic/jwt/JwtAuthenticationConverter.java
@@ -6,11 +6,12 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+import javax.annotation.Nonnull;
 import java.util.Optional;
 
 public class JwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
     @Override
-    public AbstractAuthenticationToken convert(Jwt jwt) {
+    public AbstractAuthenticationToken convert(@Nonnull Jwt jwt) {
         return new JwtAuthenticationToken(jwt,  Optional.ofNullable(jwt.getClaim("role"))
                                                         .map(Object::toString)
                                                         .map(Role::lookup)

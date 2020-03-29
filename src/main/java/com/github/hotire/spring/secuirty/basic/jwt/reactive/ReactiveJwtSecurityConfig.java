b0727c2f9b274785ab4d31b8744271347d754904
hotire <gngh0101@gmail.com>
2020-03-29 14:26:34 +0900

    Add oauth2ResourceServer of reactive security

--- src/main/java/com/github/hotire/spring/secuirty/basic/jwt/reactive/ReactiveJwtSecurityConfig.java
+++ src/main/java/com/github/hotire/spring/secuirty/basic/jwt/reactive/ReactiveJwtSecurityConfig.java
@@ -1,18 +1,30 @@
 package com.github.hotire.spring.secuirty.basic.jwt.reactive;
 
+import com.github.hotire.spring.secuirty.basic.jwt.RSAJwtDecoder;
 import com.github.hotire.spring.secuirty.basic.jwt.Role;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import javax.annotation.Nonnull;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 @EnableReactiveMethodSecurity
 public class ReactiveJwtSecurityConfig {
     @Bean
-    public SecurityWebFilterChain filter(final ServerHttpSecurity security) {
+    public SecurityWebFilterChain filter(@Nonnull final ServerHttpSecurity security, @Nonnull  @Value("${jwt.publish-key}") String publishKey) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeySpecException {
         security.csrf().disable();
         security.anonymous().authorities(Role.NONE.getAuthority());
 
-        return security.build();
+        return security.oauth2ResourceServer()
+                       .jwt()
+                       .publicKey(RSAJwtDecoder.withPublicKey(publishKey).getRsaPublicKey())
+                       .and()
+                       .and()
+                       .build();
     }
 }

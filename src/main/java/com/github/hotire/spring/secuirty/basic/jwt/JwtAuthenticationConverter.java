2331e1cd363c8a43b25c94661cad3b7c826917d0
hotire <gngh0101@gmail.com>
2020-04-06 01:43:30 +0900

    Ensure getAuthorities of roles

--- src/main/java/com/github/hotire/spring/secuirty/basic/jwt/JwtAuthenticationConverter.java
+++ src/main/java/com/github/hotire/spring/secuirty/basic/jwt/JwtAuthenticationConverter.java
@@ -7,17 +7,20 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import javax.annotation.Nonnull;
+import java.util.Collection;
 import java.util.Optional;
 
 import static com.github.hotire.spring.secuirty.basic.jwt.Role.ROLE;
+import static java.util.stream.Collectors.toSet;
 
 public class JwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
     @Override
     public AbstractAuthenticationToken convert(@Nonnull Jwt jwt) {
-        return new JwtAuthenticationToken(jwt,  Optional.ofNullable(jwt.getClaim(ROLE))
-                                                        .map(Object::toString)
-                                                        .map(Role::lookup)
-                                                        .map(Role::getAuthorities)
+        return new JwtAuthenticationToken(jwt,  Optional.ofNullable(jwt.<Collection<String>>getClaim(ROLE))
+                                                        .map(it -> it.stream()
+                                                                     .map(Role::lookup)
+                                                                     .flatMap(role -> role.getAuthorities().stream())
+                                                                     .collect(toSet()))
                                                         .orElse(Role.NONE.getAuthorities()));
     }
 }
--- src/main/java/com/github/hotire/spring/secuirty/basic/jwt/Role.java
+++ src/main/java/com/github/hotire/spring/secuirty/basic/jwt/Role.java
@@ -1,13 +1,13 @@
 package com.github.hotire.spring.secuirty.basic.jwt;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 
 import static java.util.stream.Collectors.toMap;
@@ -21,13 +21,13 @@ public enum Role {
 
     private final String role;
     private final String authority;
-    private final List<SimpleGrantedAuthority> authorities;
+    private final Set<SimpleGrantedAuthority> authorities;
     public static final String ROLE = "role";
 
     Role(final String role) {
         this.role = role;
         this.authority = "ROLE_" + role;
-        this.authorities = Lists.newArrayList(new SimpleGrantedAuthority(this.role));
+        this.authorities = Sets.newHashSet(new SimpleGrantedAuthority(this.role));
     }
 
     private static final Map<String, Role> ROLE_MAP = Arrays.stream(Role.values()).collect(toMap(Role::getRole, Function.identity()));

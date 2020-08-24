b181885c30abd854a3de9f15e6c0a8bae0a8f5d4
hotire <gngh0101@gmail.com>
2020-08-25 02:12:08 +0900

    Verify getToken

--- src/test/java/com/github/hotire/spring/secuirty/basic/jwt/JwtServiceTest.java
+++ src/test/java/com/github/hotire/spring/secuirty/basic/jwt/JwtServiceTest.java
@@ -1,37 +1,20 @@
 package com.github.hotire.spring.secuirty.basic.jwt;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.spec.InvalidKeySpecException;
 
 class JwtServiceTest {
 
     @Test
-    void jwts() throws InvalidKeySpecException, NoSuchAlgorithmException {
+    void getToken() {
+        // given
         final JwtService service = new JwtService();
 
-        final String result = service.jwts();
+        // when
+        final String result = service.getToken(JwtService.KEY);
 
-        System.out.println(result);
+        // then
+        Assertions.assertThat(result).isNotEmpty();
     }
 
-    @Test
-    void privateKey() throws InvalidKeySpecException, NoSuchAlgorithmException {
-        final JwtService service = new JwtService();
-
-        final PrivateKey result = service.privateKey();
-
-        System.out.println(result);
-    }
-
-    @Test
-    void decodePrivateKey() throws InvalidKeySpecException, NoSuchAlgorithmException {
-        final JwtService service = new JwtService();
-
-        final PrivateKey result = service.decodePrivateKey(service.key.replaceAll("(\r\n|\r|\n|\n\r)", ""));
-
-        System.out.println(result);
-    }
 }

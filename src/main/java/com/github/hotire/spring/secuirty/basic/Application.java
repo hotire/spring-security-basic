1899e938e3e5f0a47a233cc7e0717dde51bee203
hotire <gngh0101@gmail.com>
2021-09-02 00:47:45 +0900

    Simplify

--- src/main/java/com/github/hotire/spring/secuirty/basic/Application.java
+++ src/main/java/com/github/hotire/spring/secuirty/basic/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityBasicApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringSecurityBasicApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
